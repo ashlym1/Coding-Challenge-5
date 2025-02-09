57fad28ec646ae6c1b296218baa6b69aa4f95e4e
ashlym1 <ashlym3@usf.edu>
2025-02-09 17:31:12 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1: Object Properties 
+const customerInfo={
+    name:"John Doe",
+    age:35,
+    email:"john.doe@email.com" //fixed email typo 
+}; //Declaring a customer object 
+
+console.log(`Customer Name:${customerInfo.name}`); // Logging each property indidually.
+console.log(`Customer Age:${customerInfo.age}`);
+console.log(`Customer Email: ${customerInfo.email}`);
+
